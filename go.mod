e4b19a188558cc5995ccb2c6ae808054110517ca
Ralph van der Meulen <rmeulen@users.noreply.github.com>
2019-07-11 23:54:02 +0200

    First version created

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/rmeulen/go-fileserver
+
+go 1.12
---
+++ main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+)
+
+const portEnvName = "PORT"
+const defaultPort = 8080
+
+const fileRootEnvName = "FILE_ROOT"
+const defaultFileRoot = "./"
+
+func main() {
+
+	port := determinePort()
+	fileRoot := determineFileRoot()
+
+	fileServer := http.FileServer(http.Dir(fileRoot))
+
+	log.Printf("Listening on port: %d", port)
+	log.Printf("Using file root: %s", fileRoot)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), fileServer))
+
+}
+
+func determinePort() int {
+	portEnvValue := os.Getenv(portEnvName)
+
+	if portEnvValue == "" {
+		log.Printf("No value found for environment variable %s.", portEnvName)
+		log.Printf("Using default port number: %d", defaultPort)
+		return defaultPort
+	}
+
+	port, err := strconv.Atoi(portEnvValue)
+
+	if err != nil {
+		log.Printf("Invalid value for environment variable %s", portEnvName)
+		log.Printf("Defaulting to port number: %d", defaultPort)
+		return defaultPort
+	}
+
+	log.Printf("Valid environment variable %s found: %d", portEnvName, port)
+	return port
+}
+
+func determineFileRoot() string {
+	fileRoot := os.Getenv(fileRootEnvName)
+
+	if fileRoot == "" {
+		log.Printf("No value found for environment variable %s", fileRootEnvName)
+		log.Printf("Defaulting to file root: %s", defaultFileRoot)
+		fileRoot = defaultFileRoot
+	}
+
+	log.Printf("Valid environment variable %s found: %s", fileRootEnvName, fileRoot)
+	return fileRoot
+}
